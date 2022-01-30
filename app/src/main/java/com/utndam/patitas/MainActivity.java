a2e07e19d2b472262d4feb5a7eb4c6531ce7dc14
fidel <fidelmasso@gmail.com>
2022-01-30 00:32:03 -0300

    Corrige bug con permisos de map

--- app/src/main/java/com/utndam/patitas/MainActivity.java
+++ app/src/main/java/com/utndam/patitas/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawer;
     BottomNavigationView bottomBar;
+    NavigationBarView bottomBar2;
     FragmentManager fragmentManager;
     HomePerdidosFragment homePerdidos;
     BlankFragment blankFrag;
-//    MapsFragment mapaFrag;
-    Mapa1 mapaFrag2;
+    MapsFragment2 mapaFrag;
     Fragment loginFrag;
 
     @Override
@@ -35,8 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         homePerdidos = new HomePerdidosFragment();
         blankFrag = new BlankFragment();
-//        mapaFrag = new MapsFragment();
-        mapaFrag2 = new Mapa1();
+        mapaFrag = new MapsFragment2();
         loginFrag = new SignInFragment();
 
 
@@ -69,8 +69,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_toolbar_superior,menu);
 
+
+        bottomBar2 = findViewById(R.id.bottom_toolbar);
+
         bottomBar = findViewById(R.id.bottom_toolbar);
-        Menu bottomMenu = bottomBar.getMenu();
+        Menu bottomMenu = bottomBar2.getMenu();
         getMenuInflater().inflate(R.menu.menu_toolbar_inferior, bottomMenu);
 //        for (int i = 0; i < bottomMenu.size(); i++) {
 //            bottomMenu.getItem(i).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
@@ -81,8 +84,8 @@ public class MainActivity extends AppCompatActivity {
 //            });
 //        }
 
-        bottomBar.setSelectedItemId(R.id.boton_home);
-        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomBar2.setSelectedItemId(R.id.boton_home);
+        bottomBar2.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
@@ -114,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.boton_ajustes:
                         toolbar.setTitle("Ajustes");
                         fragmentManager.beginTransaction()
-                                .replace(R.id.contenedor_fragmento,mapaFrag2)
+                                .replace(R.id.contenedor_fragmento,mapaFrag)
 //                                .addToBackStack(null)
                                 .commit();
                         break;
---
+++ app/src/main/java/com/utndam/patitas/MapsFragment2.java
@@ -0,0 +1,206 @@
+package com.utndam.patitas;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MapsFragment2 extends Fragment {
+
+    private GoogleMap mapa;
+    private FusedLocationProviderClient fusedLocationClient;
+
+
+
+    private ActivityResultLauncher<String[]> requestPermissionLauncher;
+
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mapa = googleMap;
+            mapa.getUiSettings().setZoomControlsEnabled(true);
+            mapa.getUiSettings().setTiltGesturesEnabled(false);
+            LatLngBounds limites_argentina = new LatLngBounds( new LatLng(-54.964913124446696, -74.26678541029585),new LatLng(-21.897337, -54.118911));
+
+            mapa.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+                @Override
+                public void onCameraMove() {
+                    LatLng latLng = mapa.getCameraPosition().target;
+                    Log.d(null,latLng.latitude+" "+ latLng.longitude);
+                }
+
+            });
+            mapa.getUiSettings().setMyLocationButtonEnabled(true);
+
+//            mapa.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+//                @Override
+//                public void onMapLoaded() {
+//
+//                }
+//            });
+            try {
+                mapa.moveCamera(CameraUpdateFactory.newLatLngBounds(limites_argentina,0));
+            }catch (Exception e){
+                Log.d(null,e.toString());
+            }
+            probarMoverMapaAUbicacionActual();
+
+        }
+    };
+
+    public MapsFragment2() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+         requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
+            if (isGranted.containsValue(false)) {
+                Toast.makeText(getContext(),"No teni permisos",Toast.LENGTH_LONG).show();
+            } else {
+                moverMapaAUbicacionActual();
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    public void moverMapaAUbicacionActual(){
+        mapa.setMyLocationEnabled(true);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener( new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            LatLng ubicacionUsuario = new LatLng(location.getLatitude(),location.getLongitude());
+                            mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(ubicacionUsuario,15),3,null);
+                        }
+                    }
+                });
+
+
+    }
+
+
+
+    public void probarMoverMapaAUbicacionActual(){
+
+        if (tienePermisoAcceso()) {
+            moverMapaAUbicacionActual();
+        } else {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+                    || shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                explicarPermisos();
+            }
+            else{
+                pedirPermisos();
+            }
+        }
+
+    }
+
+    private void explicarPermisos() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        builder
+                .setCancelable(false)
+                .setTitle("NECESITO LOS PERMISOS !!!!")
+                .setMessage("es por la ciencia")
+//                .setPositiveButton("Dale!", (dialog, id) -> Toast.makeText( getContext(), "aca deberia pedir los permisos ", Toast.LENGTH_LONG).show())
+                .setPositiveButton("Dale!", new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        pedirPermisos();
+                    }
+                })
+                .setNegativeButton("No!", (dialog, id) -> { dialog.dismiss();Toast.makeText( getContext(), " Vos te lo perdes ", Toast.LENGTH_LONG).show();});
+
+        AlertDialog dialogo = builder.create();
+        dialogo.show();
+
+    }
+
+    private void pedirPermisos() {
+        requestPermissionLauncher.launch(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION});
+
+    }
+
+
+    private Boolean tienePermisoAcceso(){
+        return (Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                || (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED));
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
